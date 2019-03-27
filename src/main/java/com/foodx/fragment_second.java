5d1906006f4f8535818a76221cab58c63b1e5ae9
Tiago Eduardo <tiago.eduardo@ibm.com>
2019-03-27 16:28:22 -0300

    Primeira versao do app food x

---
+++ src/main/java/com/foodx/fragment_second.java
@@ -0,0 +1,32 @@
+package com.foodx;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment_second extends Fragment {
+
+    //private FragmentSecondViewModel mViewModel;
+
+    public static fragment_second newInstance() {
+        return new fragment_second();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //mViewModel = ViewModelProviders.of(this).get(FragmentSecondViewModel.class);
+    }
+
+}
